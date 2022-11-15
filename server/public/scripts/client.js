e3017877f2bf0ed67f6fbfc048e49db7b30745f5
Victor Llapa <victorllapa@gmail.com>
2022-11-14 22:43:33 -0600

    added the ability to input the users name then it will create the fields for the user to start adding to their TO DO LIST

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,27 +5,41 @@ let username = '';
 function onReady() {
 	console.log('jQuery is Loaded...');
 	$('#userNameSubmit-btn').on('click', submitName);
+	$('#userNameSubmit-btn').on('click', createTaskField);
 
 
-	$('#enter-btn').on('click', createTable);
+	// $('#enter-btn').on('click', createTaskField);
 }
 /**This function will create a header with the user's 
 *name in the header To Do list that was provided in the *input field. Once the event listener on the submit *button,  the previous content for that section will be *removed and be replaced with "username" To Do List.
  */
 function submitName() {
-	console.log('...inside the sumbitName function.');
+	console.log('...inside the submitName function.');
 	username = $('#usernameInput').val();
 
 	console.log('The user\'s name is: ', username);
 
-	$('.userList').empty();
-	$('.userList').html(`
+	$('#userList').empty();
+	$('#userList').append(`
 	<h1 id="userHeader" > ${username}'s To Do List	</h1>
 `);
 }
 
-function renderList() {
+function renderList(list) {
 	$('#viewTable').empty();
+	for (let i = 0; i < list.length; i++) {
+		let list = list[i];
+		$('#viewTable').append(`
+	<tr>
+		<td> ${list.description}</td>
+		<td> ${list.isComplete}</td>
+		<button id = "delete-btn"> </button>
+	</tr>
+`);
+
+
+
+	}
 
 }
 
@@ -35,8 +49,7 @@ function createTaskField() {
 	$('.taskSection').append(`
 		<tr>
 			<th> Task</th>
-			<th> Complete</th>
-			<th> Delete</th>
+			<th> Status</th>
 		</tr>
 `);
 	$('.taskSection').append(`
@@ -44,16 +57,20 @@ function createTaskField() {
 		<td>
 			<input id="taskIn" type="text" placeholder="Description">	
 		</td>
+		<td>
+			<button id = "isComplete">  NOT COMPLETE </button>
+		</td>
+		<td>
+			<button id ="delete-button"> DELETE </button>
+		</td>
 	</tr>
 `);
-	saveTask()
 }
 function saveTask() {
 	console.log('in the saveTask function');
 
 	let newTask = {
 		description: $('#taskIn').val(),
-		complete: ""
 	};
 	console.log('Task preparing to send to DataBase : ', newTask);
 	$.ajax({
@@ -66,6 +83,19 @@ function saveTask() {
 	}).catch(function (error) {
 		console.log('Error: ', error);
 		alert('Was not able to POST to database...');
+	});
+}
+
+function getAllTasks() {
+	console.log('in getKoalas');
+	$.ajax({
+		method: 'GET',
+		url: '/task'
+	}).then(function (response) {
+		console.log('Response is: ', response);
+		renderList(response);
+	}).catch(function (error) {
+		console.log('Error in retrieving the data from database');
 	});
 }
 
